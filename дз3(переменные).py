d5c98cdb9e208726468100f19f446787fddf915a
LilChich4 <cfyg456@mail.ru>
2024-08-29 00:52:35 +0300

    Add files via upload

---
+++ дз3(переменные).py
@@ -0,0 +1,4 @@
+The_number_of_completed_HW ='12'
+Number_of_hours_spent ='1.5'
+Course_name ='Python'
+print('Курс:',Course_name,',''Всего задач:',The_number_of_completed_HW,',''Затрачено часов:',Number_of_hours_spent,',''Среднее время выполнения:',float(Number_of_hours_spent)/int(The_number_of_completed_HW))
